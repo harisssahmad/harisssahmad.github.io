6ad3eb30a4591a3fc8643ef0f67aa6912a9846ee
harisssahmad <39519497+harisssahmad@users.noreply.github.com>
2018-10-26 23:35:07 +0500

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -1543,24 +1543,3 @@ ScrollReveal();
 return ScrollReveal;
 
 })));
-
-ScrollReveal().reveal('.responsive-img');
-ScrollReveal().reveal('.logo');
-ScrollReveal().reveal('.social-icon');
-ScrollReveal().reveal('#Basic');
-ScrollReveal().reveal('#Pro');
-ScrollReveal().reveal('#Ultimate');
-ScrollReveal().reveal('#Logo');
-ScrollReveal().reveal('#Logo-two');
-ScrollReveal().reveal('#Banner');
-ScrollReveal().reveal('#behance');
-ScrollReveal().reveal('#facebook', { delay: 500 });
-ScrollReveal().reveal('#instagram', { delay: 600 });
-ScrollReveal().reveal('#twitter', { delay: 700 });
-ScrollReveal().reveal('.heading');
-ScrollReveal().reveal('.subheading');
-ScrollReveal().reveal('.img-one');
-ScrollReveal().reveal('.img-two');
-ScrollReveal().reveal('#heading');
-ScrollReveal().reveal('#subheading', { delay: 500 });
-ScrollReveal().reveal('#responsive-img', { delay: 800 });
