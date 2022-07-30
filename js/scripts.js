7634911ba085566c517b1e8ef796b5c269288101
harisssahmad <39519497+harisssahmad@users.noreply.github.com>
2022-07-31 02:34:09 +0500

    Update Website Content

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,3 @@
-      var example = ['website', 'logo', 'banner', 'birthday card', 't-shirt'];
-
-        textSequence(0);
-        function textSequence(i) {
-
-            if (example.length > i) {
-                setTimeout(function() {
-                    document.getElementById("sequence").innerHTML = example[i];
-                    textSequence(++i);
-                }, 1000); // 1 second (in milliseconds)
-
-            } else if (example.length == i) { // Loop
-                textSequence(0);
-            }
-
-        }
-
     function displayNone(){
         if(document.getElementById('nav-check').checked == false) {
             document.getElementById('nav-check').checked = true;
