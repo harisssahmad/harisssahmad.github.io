326e2eaad97c800bfbfbf43a258fc294d9f6c944
Haris Ahmad <me.haris2003@gmail.com>
2024-09-09 17:38:33 +0500

    Update website with new project mentions
    Changed uni's current year
    Added Tutona in projects
    Updated introduction

--- js/animate.js
+++ js/animate.js
@@ -1,137 +1,185 @@
 var pageWidth = document.body.clientWidth;
 
 // Fade in INITIAL Left and Right for SM Screen Size
-const animateInitSMLeft = document.querySelectorAll('.animate-init-sm-left')
-const animateInitSMRight = document.querySelectorAll('.animate-init-sm-right')
+const animateInitSMLeft = document.querySelectorAll(".animate-init-sm-left");
+const animateInitSMRight = document.querySelectorAll(".animate-init-sm-right");
 
 // Fade in INITIAL simple for SM Screen Size
-const animateInitSMSimple = document.querySelectorAll('.animate-init-sm-simple')
+const animateInitSMSimple = document.querySelectorAll(
+    ".animate-init-sm-simple"
+);
 
 // Fade in INITIAL Left and Right for LG Screen Size
-const animateInitLGLeft = document.querySelectorAll('.animate-init-lg-left')
-const animateInitLGRight = document.querySelectorAll('.animate-init-lg-right')
+const animateInitLGLeft = document.querySelectorAll(".animate-init-lg-left");
+const animateInitLGRight = document.querySelectorAll(".animate-init-lg-right");
 
 // Fade in INITIAL simple for LG Screen Size
-const animateInitLGSimple = document.querySelectorAll('.animate-init-lg-simple')
+const animateInitLGSimple = document.querySelectorAll(
+    ".animate-init-lg-simple"
+);
 
 // Fade in all directions and simple regardless of screen size
-const animateElementsLeft = document.querySelectorAll('.animate-left')
-const animateElementsRight = document.querySelectorAll('.animate-right')
-const animateElementsDown = document.querySelectorAll('.animate-down')
-const animateElementsUp = document.querySelectorAll('.animate-up')
-const animateElementsSimple = document.querySelectorAll('.animate-simple')
+const animateElementsLeft = document.querySelectorAll(".animate-left");
+const animateElementsRight = document.querySelectorAll(".animate-right");
+const animateElementsDown = document.querySelectorAll(".animate-down");
+const animateElementsUp = document.querySelectorAll(".animate-up");
+const animateElementsSimple = document.querySelectorAll(".animate-simple");
 
 // Fade in all directions and simple for LG screen size
-const animateElementsLGLeft = document.querySelectorAll('.animate-lg-left')
-const animateElementsLGRight = document.querySelectorAll('.animate-lg-right')
-const animateElementsLGUp = document.querySelectorAll('.animate-lg-up')
-const animateElementsLGDown = document.querySelectorAll('.animate-lg-down')
-const animateElementsLGSimple = document.querySelectorAll('.animate-lg-simple')
+const animateElementsLGLeft = document.querySelectorAll(".animate-lg-left");
+const animateElementsLGRight = document.querySelectorAll(".animate-lg-right");
+const animateElementsLGUp = document.querySelectorAll(".animate-lg-up");
+const animateElementsLGDown = document.querySelectorAll(".animate-lg-down");
+const animateElementsLGSimple = document.querySelectorAll(".animate-lg-simple");
 
 // Fade in all directions and simple for SM screen size
-const animateElementsSMLeft = document.querySelectorAll('.animate-sm-left')
-const animateElementsSMRight = document.querySelectorAll('.animate-sm-right')
-const animateElementsSMUp = document.querySelectorAll('.animate-sm-up')
-const animateElementsSMDown = document.querySelectorAll('.animate-sm-down')
-const animateElementsSMSimple = document.querySelectorAll('.animate-sm-simple')
+const animateElementsSMLeft = document.querySelectorAll(".animate-sm-left");
+const animateElementsSMRight = document.querySelectorAll(".animate-sm-right");
+const animateElementsSMUp = document.querySelectorAll(".animate-sm-up");
+const animateElementsSMDown = document.querySelectorAll(".animate-sm-down");
+const animateElementsSMSimple = document.querySelectorAll(".animate-sm-simple");
 
 let prevScrollPos = window.pageYOffset;
 
 const isScrolledIntoView = (element) => {
-  const elementTop = element.getBoundingClientRect().top;
-  const elementHeight = element.offsetHeight;
-  const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const elementHeight = element.offsetHeight;
+    const windowHeight = window.innerHeight;
 
-  return elementTop + elementHeight >= 0 && elementTop <= windowHeight;
-}
+    return elementTop + elementHeight >= 0 && elementTop <= windowHeight;
+};
 
 const checkAndAddAnimation = (element, addClass) => {
-  if (isScrolledIntoView(element) && !element.classList.contains(addClass)) {
-    element.classList.add(addClass);
-  }
-  else if (!isScrolledIntoView(element) && element.classList.contains(addClass)) {
-    element.classList.remove(addClass);
-  }
-}
+    if (isScrolledIntoView(element) && !element.classList.contains(addClass)) {
+        element.classList.add(addClass);
+    } else if (
+        !isScrolledIntoView(element) &&
+        element.classList.contains(addClass)
+    ) {
+        element.classList.remove(addClass);
+    }
+};
 
 if (pageWidth <= 991) {
-  // initial left sm elements
-  animateInitSMLeft.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInLeft'));
+    // initial left sm elements
+    animateInitSMLeft.forEach((element) =>
+        checkAndAddAnimation(element, "animate__fadeInLeft")
+    );
 
-  // initial right sm elements
-  animateInitSMRight.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInRight'));
+    // initial right sm elements
+    animateInitSMRight.forEach((element) =>
+        checkAndAddAnimation(element, "animate__fadeInRight")
+    );
 
-  // initial simple sm elements
-  animateInitSMSimple.forEach((element) => checkAndAddAnimation(element, 'animate__fadeIn'));
+    // initial simple sm elements
+    animateInitSMSimple.forEach((element) =>
+        checkAndAddAnimation(element, "animate__fadeIn")
+    );
 } else {
-  // initial left sm elements
-  animateInitLGLeft.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInLeft'));
+    // initial left sm elements
+    animateInitLGLeft.forEach((element) =>
+        checkAndAddAnimation(element, "animate__fadeInLeft")
+    );
 
-  // initial right sm elements
-  animateInitLGRight.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInRight'));
+    // initial right sm elements
+    animateInitLGRight.forEach((element) =>
+        checkAndAddAnimation(element, "animate__fadeInRight")
+    );
 
-  // initial simple lg elements
-  animateInitLGSimple.forEach((element) => checkAndAddAnimation(element, 'animate__fadeIn'));
+    // initial simple lg elements
+    animateInitLGSimple.forEach((element) =>
+        checkAndAddAnimation(element, "animate__fadeIn")
+    );
 }
 
 const handleScroll = () => {
-  const currentScrollPos = window.pageYOffset;
+    const currentScrollPos = window.scrollY || window.pageYOffset;
 
-  if (currentScrollPos > prevScrollPos) {
-    // Scrolling down
+    if (currentScrollPos > prevScrollPos) {
+        // Scrolling down
 
-    // left elements
-    animateElementsLeft.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInLeft'));
-    
-    // right elements
-    animateElementsRight.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInRight'));
+        // left elements
+        animateElementsLeft.forEach((element) =>
+            checkAndAddAnimation(element, "animate__fadeInLeft")
+        );
 
-    // down elements
-    animateElementsDown.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInDown'));
+        // right elements
+        animateElementsRight.forEach((element) =>
+            checkAndAddAnimation(element, "animate__fadeInRight")
+        );
 
-    // up elements
-    animateElementsUp.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInUp'));
+        // down elements
+        animateElementsDown.forEach((element) =>
+            checkAndAddAnimation(element, "animate__fadeInDown")
+        );
 
-	// simple elements
-    animateElementsSimple.forEach((element) => checkAndAddAnimation(element, 'animate__fadeIn'));
+        // up elements
+        animateElementsUp.forEach((element) =>
+            checkAndAddAnimation(element, "animate__fadeInUp")
+        );
 
-    // LG size
-    if(pageWidth > 991) {
-      // left elements
-      animateElementsLGLeft.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInLeft'));
-      
-      // right elements
-      animateElementsLGRight.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInRight'));
+        // simple elements
+        animateElementsSimple.forEach((element) =>
+            checkAndAddAnimation(element, "animate__fadeIn")
+        );
 
-      // up elements
-      animateElementsLGUp.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInUp'));
+        // LG size
+        if (pageWidth > 991) {
+            // left elements
+            animateElementsLGLeft.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInLeft")
+            );
 
-      // down elements
-      animateElementsLGDown.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInDown'));
+            // right elements
+            animateElementsLGRight.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInRight")
+            );
 
-	  // simple elements
-      animateElementsLGSimple.forEach((element) => checkAndAddAnimation(element, 'animate__fadeIn'));
+            // up elements
+            animateElementsLGUp.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInUp")
+            );
 
-    } else { // SM size
+            // down elements
+            animateElementsLGDown.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInDown")
+            );
 
-      // left elements
-      animateElementsSMLeft.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInLeft'));
-      
-      // right elements
-      animateElementsSMRight.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInRight'));
+            // simple elements
+            animateElementsLGSimple.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeIn")
+            );
+        } else {
+            // SM size
 
-	  // up elements
-      animateElementsSMUp.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInUp'));
+            // left elements
+            animateElementsSMLeft.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInLeft")
+            );
 
-      // down elements
-      animateElementsSMDown.forEach((element) => checkAndAddAnimation(element, 'animate__fadeInDown'));
+            // right elements
+            animateElementsSMRight.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInRight")
+            );
 
-	  // simple elements
-      animateElementsSMSimple.forEach((element) => checkAndAddAnimation(element, 'animate__fadeIn'));
+            // up elements
+            animateElementsSMUp.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInUp")
+            );
+
+            // down elements
+            animateElementsSMDown.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeInDown")
+            );
+
+            // simple elements
+            animateElementsSMSimple.forEach((element) =>
+                checkAndAddAnimation(element, "animate__fadeIn")
+            );
+        }
     }
-  }
 
-  prevScrollPos = currentScrollPos;
-}
+    prevScrollPos = currentScrollPos;
+};
 
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,118 @@
-// For the navbar (opacity change on scroll)
-
+/**
+ * Adjusts the opacity of the navigation bar based on the scroll position.
+ * Updates the background color of the navigation bar to reflect the scroll position.
+ */
 const navOpacity = () => {
-  var navbarBackground = document.getElementById('navbar');
-  var scrollPosition = window.scrollY;
+    var navbarBackground = document.getElementById("navbar");
+    var scrollPosition = window.scrollY;
 
-  if (scrollPosition < 600) {
-    var opacity = scrollPosition / 600; // Calculate opacity based on scroll position
+    if (scrollPosition < 600) {
+        var opacity = scrollPosition / 600; // Calculate opacity based on scroll position
 
-    // Cap opacity to a maximum of 1
-    if (opacity > 1) {
-      opacity = 1;
+        // Cap opacity to a maximum of 1
+        if (opacity > 1) {
+            opacity = 1;
+        }
+
+        navbarBackground.style.backgroundColor =
+            "rgba(255, 255, 255, " + opacity + ")";
+    } else {
+        navbarBackground.style.backgroundColor = "rgba(255, 255, 255, 1)";
     }
+};
 
-    navbarBackground.style.backgroundColor = 'rgba(255, 255, 255, ' + opacity + ')';
-  } else {
-    navbarBackground.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', navOpacity);
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", navOpacity);
 });
 
 navOpacity();
 
-// For the navbar (hide on scroll)
-
+/**
+ * Function to handle the behavior of the navbar on scroll.
+ * It hides the navbar when scrolling down and shows it when scrolling up.
+ * Adjust the threshold value to control when the navbar should hide/show.
+ */
 var prevScrollpos = window.scrollY || document.documentElement.scrollTop;
 var navbar = document.getElementById("navbar");
-var threshold = 94; // Adjust this value as needed
+var threshold = 94;
 
-window.onscroll = function() {
-  var currentScrollPos = window.scrollY || document.documentElement.scrollTop;
-  var scrollDifference = Math.abs(currentScrollPos - prevScrollpos);
-  
-  if (scrollDifference >= threshold) {
-    if (prevScrollpos > currentScrollPos) {
-      navbar.style.top = "0";
-    } else {
-      navbar.style.top = "-94px";
+window.onscroll = function () {
+    var currentScrollPos = window.scrollY || document.documentElement.scrollTop;
+    var scrollDifference = Math.abs(currentScrollPos - prevScrollpos);
+
+    if (scrollDifference >= threshold) {
+        if (prevScrollpos > currentScrollPos) {
+            navbar.style.top = "0";
+        } else {
+            navbar.style.top = "-94px";
+        }
+
+        prevScrollpos = currentScrollPos;
     }
-    
-    prevScrollpos = currentScrollPos;
-  }
 };
 
-// Function that closes the navbar when navbar link is clicked on sm devices:
-
+/**
+ * Toggles the visibility of the navigation menu by checking and unchecking the "nav-check" checkbox element.
+ * If the checkbox is unchecked, it gets checked. If it is checked, it gets unchecked and triggers the toggleNav function.
+ */
 const hideNav = () => {
-    if(document.getElementById('nav-check').checked == false) {
-        document.getElementById('nav-check').checked = true;
-    }
-    else if(document.getElementById('nav-check').checked == true) {
-        document.getElementById('nav-check').checked = false;
+    if (document.getElementById("nav-check").checked == false) {
+        document.getElementById("nav-check").checked = true;
+    } else if (document.getElementById("nav-check").checked == true) {
+        document.getElementById("nav-check").checked = false;
         toggleNav();
-    } 
-}
+    }
+};
 
-const smNavToggler = document.getElementById('nav-check');
-const smNavLinks = document.getElementById('sm-nav-links');
-const smNavLines = document.getElementsByClassName('line')
+/**
+ * Retrieves the elements needed for small navigation functionality:
+ * - smNavToggler: Element with the id "nav-check"
+ * - smNavLinks: Element with the id "sm-nav-links"
+ * - smNavLines: Collection of elements with the class "line"
+ */
+const smNavToggler = document.getElementById("nav-check");
+const smNavLinks = document.getElementById("sm-nav-links");
+const smNavLines = document.getElementsByClassName("line");
 
-// Function that toggles nav on sm devices:
-
+/**
+ * Function that toggles the navigation menu on small devices.
+ * Toggles the visibility of navigation links and applies transformations to navigation lines based on the state of the navigation toggler.
+ */
 const toggleNav = () => {
-    if(smNavToggler.checked) {
-        if(smNavLinks.classList.contains('hide-links')) {
-            smNavLinks.classList.remove('hide-links')
-            smNavLinks.classList.add('show-links')
+    if (smNavToggler.checked) {
+        if (smNavLinks.classList.contains("hide-links")) {
+            smNavLinks.classList.remove("hide-links");
+            smNavLinks.classList.add("show-links");
         }
-        for(var i = 0; i < smNavLines.length; i++) {
-            smNavLines[i].classList.add('line' + (i+1) + '-transform')
+        for (var i = 0; i < smNavLines.length; i++) {
+            smNavLines[i].classList.add("line" + (i + 1) + "-transform");
         }
     } else {
-        if(smNavLinks.classList.contains('show-links')) {
-            smNavLinks.classList.remove('show-links')
-            smNavLinks.classList.add('hide-links')
+        if (smNavLinks.classList.contains("show-links")) {
+            smNavLinks.classList.remove("show-links");
+            smNavLinks.classList.add("hide-links");
         }
-        for(var i = 0; i < smNavLines.length; i++) {
-            smNavLines[i].classList.remove('line' + (i+1) + '-transform')
+        for (var i = 0; i < smNavLines.length; i++) {
+            smNavLines[i].classList.remove("line" + (i + 1) + "-transform");
         }
     }
-}
+};
+
+/**
+ * Listens for the DOMContentLoaded event and enables horizontal scrolling of a grid element
+ * within the ".tools-and-tech > .tools-and-tech-container > .grid" structure based on the user's wheel actions.
+ * Scrolls to the right when the user scrolls up and to the left when the user scrolls down.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    const grid = document.querySelector(
+        ".tools-and-tech > .tools-and-tech-container > .grid"
+    );
+
+    grid.addEventListener("wheel", function (event) {
+        if (event.deltaY !== 0) {
+            // Scroll to the right if the user scrolls up, and to the left if the user scrolls down
+            grid.scrollLeft += event.deltaY * 5;
+            event.preventDefault();
+        }
+    });
+});
